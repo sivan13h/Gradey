0e8eaf6a85d9ea8fe0805ac5a6166f4f5431eba9
Sivan Hart <sivan13h@gmail.com>
2021-05-17 19:54:08 +0200

    change to gradey api

--- src/services/dataMethods.js
+++ src/services/dataMethods.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const getGrades = async () => {
-  const response = await axios.get("http://localhost:8000/grades");
+  const response = await axios.get("https://gradey-api.herokuapp.com/grades");
   return response.data;
 };
 
 export const addGrade = async (newGrade) => {
-  await axios.post("http://localhost:8000/grades", {
+  await axios.post("https://gradey-api.herokuapp.com/grades", {
     name: newGrade.name,
     semester: newGrade.semester,
     grade: newGrade.grade,
@@ -15,9 +15,12 @@ export const addGrade = async (newGrade) => {
 };
 
 export const updateGrade = async (gradeId, updatedGrade) => {
-  await axios.patch(`http://localhost:8000/grades/${gradeId}`, updatedGrade);
+  await axios.patch(
+    `https://gradey-api.herokuapp.com/grades${gradeId}`,
+    updatedGrade
+  );
 };
 
 export const deleteGrade = async (gradeId) => {
-  await axios.delete(`http://localhost:8000/grades/${gradeId}`);
+  await axios.delete(`https://gradey-api.herokuapp.com/grades${gradeId}`);
 };
